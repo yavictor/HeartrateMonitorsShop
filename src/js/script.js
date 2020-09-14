8146e21841b422d084917497f68bf35e51dac246
yv <yavictor37@yandex.ru>
2020-09-14 12:49:17 +0300

    Add page-up arrow and smoothe scroll animation for it

--- src/js/script.js
+++ src/js/script.js
@@ -67,4 +67,21 @@ $(document).ready(function(){
     });
     return false;
   });
+
+  //scroll
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.page-up').fadeIn();
+    } else {
+      $('.page-up').fadeOut();
+    }
+  });
+
+
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+
 });
