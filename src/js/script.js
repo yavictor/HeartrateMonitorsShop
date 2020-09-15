e4e4efd9a4ea24d85634a918396f308e28917e1e
yv <yavictor37@yandex.ru>
2020-09-15 20:48:56 +0300

    Optimising site, add html and image minimiser to gulp tasks

--- src/js/script.js
+++ src/js/script.js
@@ -78,7 +78,7 @@ $(document).ready(function(){
   });
 
 
-  $("a[href^='#']").click(function(){
+  $("a[href=#").click(function(){
     const _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
